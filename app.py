5e1b6039a1eceb7e1eca641564e39bb2ccb5d4e4
Kep <KepCreations@gmail.com>
2021-04-21 22:23:47 -0400

    /artist working

--- app.py
+++ app.py
@@ -319,16 +319,23 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  # data=[{
+  #   "id": 4,
+  #   "name": "Guns N Petals",
+  # }, {
+  #   "id": 5,
+  #   "name": "Matt Quevedo",
+  # }, {
+  #   "id": 6,
+  #   "name": "The Wild Sax Band",
+  # }]
+
+  data=[]
+  artists = Artist.query.order_by(Artist.name).all()
+
+  for artist in artists:
+    data.append({"id":artist.id, "name":artist.name})
+
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
@@ -589,7 +596,7 @@ def create_show_submission():
   except:
     db.session.rollback()
     print(sys.exc_info())
-    
+
     # TODO: on unsuccessful db insert, flash an error instead.
     flash('An error occurred. Show could not be listed.')
   finally:
