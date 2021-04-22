7be127cabc04764bc0436073551fd2c0ceba9ad6
Kep <KepCreations@gmail.com>
2021-04-22 14:53:44 -0400

    edit_artist working

--- app.py
+++ app.py
@@ -534,19 +534,47 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
+  # artist={
+  #   "id": 4,
+  #   "name": "Guns N Petals",
+  #   "genres": ["Rock n Roll"],
+  #   "city": "San Francisco",
+  #   "state": "CA",
+  #   "phone": "326-123-5000",
+  #   "website": "https://www.gunsnpetalsband.com",
+  #   "facebook_link": "https://www.facebook.com/GunsNPetals",
+  #   "seeking_venue": True,
+  #   "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
+  #   "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
+  # }
+
+  data = Artist.query.filter_by(id=artist_id).first()
+
   artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
+    "id": data.id,
+    "name": data.name,
+    "genres": data.genres,
+    "city": data.city,
+    "state": data.state,
+    "phone": data.phone,
+    "website": data.website,
+    "facebook_link": data.facebook_link,
+    "seeking_venue": data.seeking_venue,
+    "seeking_description": data.seeking_description,
+    "image_link": data.image_link
   }
+  # populate form with current data
+  form.name.process_data(artist['name'])
+  form.genres.process_data(artist['genres'])
+  form.city.process_data(artist['city'])
+  form.state.process_data(artist['state'])
+  form.phone.process_data(artist['phone'])
+  form.website_link.process_data(artist['website'])
+  form.facebook_link.process_data(artist['facebook_link'])
+  form.seeking_venue.process_data(artist['seeking_venue'])
+  form.seeking_description.process_data(artist['seeking_description'])
+  form.image_link.process_data(artist['image_link'])
+  
   # TODO: populate form with fields from artist with ID <artist_id>
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
